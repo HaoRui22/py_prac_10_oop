ae39bce788021bb03e4ab9153ffbcc06f5275379
Zane Li <zaneli01@qq.com>
2025-11-21 22:31:30 +0800

    更新获取对象类型一节部分内容代码

--- get_object_type.py
+++ get_object_type.py
@@ -1,3 +1,15 @@
 # 获取对象信息
+from animals import Animal
 
-# 通过 type() 判断对象类型。
+# 通过 type() 判断对象类型
+# 基本类型都可以使用 type() 判断
+print(type(123))
+print(type("hello"))
+print(type(None))
+
+# 指向函数或类的变量，也可以用 type() 判断
+print(type(abs))
+
+ani = Animal()
+print(type(ani))
+
